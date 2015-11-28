a031b2f4464b3702cb133c22bc63796f3e73114c
Steve Emmerson <stephen.t.emmerson@gmail.com>
2015-11-27 21:21:23 -0600

    First commit of web server

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+
+
+var middleware = {
+
+	requireAuthentication: function (req, res, next) {
+		console.log('private route hit!');
+		next();
+	},
+
+	logger: function (req, res, next) {
+		console.log('Request ' + req.method + ' ' + req.originalUrl + ' on ' + new Date().toString());
+		next();
+	}
+
+};
+
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us');
+});
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+
+
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT +'.');
+});
+
